89974b5c4aab1179a9f86315bf0fb251c27a9572
Henrik <>
2020-12-07 21:21:04 +0100

    Refactored Customer component

--- src/main/java/wineshop/customer/CustomerRegistrationForm.java
+++ src/main/java/wineshop/customer/CustomerRegistrationForm.java
@@ -1,51 +1,35 @@
-/*
- * Copyright 2013-2019 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package wineshop.customer;
 
 import javax.validation.constraints.NotEmpty;
 
 public class CustomerRegistrationForm {
 
-	@NotEmpty(message = "{RegistrationForm.address.NotEmpty}") // s
-	private final String firstname;
+	@NotEmpty
+	private final String firstName;
 
-	@NotEmpty(message = "{RegistrationForm.password.NotEmpty}") //
-	private final String lastname;
+	@NotEmpty
+	private final String familyName;
 
-	@NotEmpty(message = "{RegistrationForm.name.NotEmpty}") //
+	@NotEmpty
 	private final String email;
 
-	@NotEmpty(message = "{RegistrationForm.name.NotEmpty}") //
+	@NotEmpty
 	private final String address;
 
 
-	public CustomerRegistrationForm(String firstname, String lastname, String email, String address) {
-		this.firstname = firstname;
-		this.lastname = lastname;
+	public CustomerRegistrationForm(String firstName, String familyName, String email, String address) {
+		this.firstName = firstName;
+		this.familyName = familyName;
 		this.email = email;
 		this.address = address;
 	}
 
-
-	public String getFirstname() {
-		return firstname;
+	public String getFirstName() {
+		return firstName;
 	}
 
-	public String getLastname() {
-		return lastname;
+	public String getFamilyName() {
+		return familyName;
 	}
 
 	public String getEmail() {
