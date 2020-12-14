db41ef8fbfa258bc16553fe31cce817a5a5e432b
Tom Uhlemann <mail.tomuhlemann@gmail.com>
2020-12-14 17:23:51 +0100

    F0210+F0211+F0220(teilweise)+F0230 implementiert

--- src/main/java/wineshop/order/OrderCustManager.java
+++ src/main/java/wineshop/order/OrderCustManager.java
@@ -4,10 +4,13 @@ import org.salespointframework.inventory.UniqueInventory;
 import org.salespointframework.inventory.UniqueInventoryItem;
 import org.salespointframework.order.Cart;
 import org.salespointframework.order.CartItem;
+import org.salespointframework.order.OrderLine;
 import org.salespointframework.order.OrderManagement;
 import org.salespointframework.payment.Cash;
 import org.salespointframework.quantity.Quantity;
 import org.salespointframework.useraccount.UserAccount;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +18,7 @@ import org.springframework.util.Assert;
 import wineshop.customer.Customer;
 import wineshop.customer.CustomerManager;
 import wineshop.wine.Wine;
+
 import java.util.Iterator;
 
 
@@ -25,6 +29,8 @@ public class OrderCustManager{
 	private final OrderManagement<OrderCust> orderManagement;
 	private final CustomerManager customerManager;
 	private final UniqueInventory<UniqueInventoryItem> inventory;
+
+	private static final Logger LOG = LoggerFactory.getLogger(OrderCustManager.class);
 
 
 	OrderCustManager(OrderCustRepository orderCustRepository, OrderManagement<OrderCust> orderManagement, CustomerManager customerManager, UniqueInventory<UniqueInventoryItem> inventory) {
@@ -78,6 +84,95 @@ public class OrderCustManager{
 				orderManagement.completeOrder(order);
 			}
 		}
+	}
+
+	public void orderCheckout(UserAccount userAccount, Cart cart, CartCustForm cartCustForm) {
+		Customer customer = customerManager.findCustomerById((Long) cartCustForm.getCustomerId());
+		var order = new OrderCust(userAccount, Cash.CASH, customer);
+		var preorder = new OrderCust(userAccount, Cash.CASH, customer);
+
+		Iterator<CartItem> item = cart.iterator();
+		do {
+			CartItem cartItem = item.next();
+			Wine wine = (Wine) cartItem.getProduct();
+			InventoryItem inventoryItem = inventory.findByProductIdentifier(wine.getId()).get();
+
+			if (!cartItem.getQuantity().isGreaterThan(inventoryItem.getQuantity())) {
+				order.addOrderLine(wine, cartItem.getQuantity());
+			} else {
+				preorder.addOrderLine(wine, cartItem.getQuantity());
+			}
+		} while (item.hasNext());
+
+		cart.clear();
+
+		if (preorder.getOrderLines().stream().count() != 0) {
+			Iterator<OrderLine> orderLineIterator = preorder.getOrderLines().get().iterator();
+			do {
+				OrderLine orderLine = orderLineIterator.next();
+				Wine wine = (Wine) inventory.findByProductIdentifier(orderLine.getProductIdentifier()).get().getProduct();
+				Quantity quantity = orderLine.getQuantity();
+
+				cart.addOrUpdateItem(wine, quantity);
+			} while (orderLineIterator.hasNext());
+		}
+
+		orderManagement.delete(preorder);
+		orderManagement.payOrder(order);
+		orderManagement.completeOrder(order);
+
+
+	}
+
+	public void preorderCheckout(UserAccount userAccount, Cart cart, CartCustForm cartCustForm) {
+		Customer customer = customerManager.findCustomerById((Long) cartCustForm.getCustomerId());
+		var preorder = new OrderCust(userAccount, Cash.CASH, customer);
+		var order = new OrderCust(userAccount, Cash.CASH, customer);
+
+		Iterator<CartItem> item = cart.iterator();
+		do {
+			CartItem cartItem = item.next();
+			Wine wine = (Wine) cartItem.getProduct();
+			InventoryItem inventoryItem = inventory.findByProductIdentifier(wine.getId()).get();
+
+			if (cartItem.getQuantity().isGreaterThan(inventoryItem.getQuantity())) {
+				preorder.addOrderLine(wine, cartItem.getQuantity());
+				inventoryItem.increaseQuantity((Quantity.of(10).subtract(inventoryItem.getQuantity())).add(cartItem.getQuantity()));
+			} else {
+				order.addOrderLine(wine, cartItem.getQuantity());
+			}
+		} while (item.hasNext());
+
+		cart.clear();
+
+		if (order.getOrderLines().stream().count() != 0) {
+			Iterator<OrderLine> orderLineIterator = order.getOrderLines().get().iterator();
+			do {
+				OrderLine orderLine = orderLineIterator.next();
+				Wine wine = (Wine) inventory.findByProductIdentifier(orderLine.getProductIdentifier()).get().getProduct();
+				Quantity quantity = orderLine.getQuantity();
+
+				cart.addOrUpdateItem(wine, quantity);
+			} while (orderLineIterator.hasNext());
+		}
+
+		orderManagement.delete(order);
+		orderManagement.payOrder(preorder);
+		orderManagement.completeOrder(preorder);
+
+	}
+
+	public void refresh(String itemId, int number, Cart cart) {
+
+		CartItem item = cart.getItem(itemId).get();
+		Wine wine = (Wine) item.getProduct();
+
+		cart.addOrUpdateItem(wine, Quantity.of(0).subtract(item.getQuantity()));
+		cart.addOrUpdateItem(wine, Quantity.of(number));
+	}
+
+	public void deleteItem(String itemId, Cart cart) {
+		cart.removeItem(itemId);
 	}
 
 	public Streamable<OrderCust> findAll() {
