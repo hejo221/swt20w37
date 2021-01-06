98ecb6fb2f7aa863b05084213703be02516c1878
Lavon Śpirydonaŭ <lavon@outlook.de>
2021-01-06 16:26:21 +0100

    CatalogIntegrationTest hinzugefügt

---
+++ src/test/java/wineshop/wine/CatalogIntegrationTest.java
@@ -0,0 +1,77 @@
+package wineshop.wine;
+
+import org.junit.jupiter.api.Test;
+import org.salespointframework.catalog.ProductIdentifier;
+import org.salespointframework.inventory.UniqueInventory;
+import org.salespointframework.inventory.UniqueInventoryItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import wineshop.AbstractIntegrationTests;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CatalogIntegrationTest extends AbstractIntegrationTests {
+
+	@Autowired
+	CatalogManager catalogManager;
+	@Autowired
+	UniqueInventory<UniqueInventoryItem> inventory;
+
+	// Anzahl aller Weinprodukte im Katalog
+	@Test
+	public void findsAllWines() {
+		Iterable<Wine> allWines = catalogManager.getAllWines();
+		assertThat(allWines).hasSize(14);
+	}
+
+	// Anzahl aller verfügbaren Produkten im Katalog
+	@Test
+	public void findsAllAvailableWines() {
+		Iterable<Wine> availableWines = catalogManager.getAvailableWines();
+		assertThat(availableWines).hasSize(9);
+	}
+
+	// Anzahl aller nicht verfügbaren Produkten im Katalog
+	@Test
+	public void findsAllUnavailableWines() {
+		Iterable<Wine> unavailableWines = catalogManager.getUnavailableWines();
+		assertThat(unavailableWines).hasSize(5);
+	}
+
+	// Macht alle verfügbare Weine nicht verfügbar
+	@Test
+	public void doesFromAllAvailableUnavailable() {
+		for (Wine wine : catalogManager.getAvailableWines())
+			catalogManager.makeItemUnavailable(wine);
+
+		assertThat(catalogManager.getAllWines()).hasSize(14);
+		assertThat(catalogManager.getAvailableWines()).hasSize(0);
+		assertThat(catalogManager.getUnavailableWines()).hasSize(14);
+	}
+
+	// Macht alle nicht verfügbare Weine (wieder) verfügbar
+	@Test
+	public void doesFromAllUnavailableAvailable() {
+		for (Wine wine : catalogManager.getUnavailableWines())
+			catalogManager.makeItemAvailable(wine);
+
+		assertThat(catalogManager.getAllWines()).hasSize(14);
+		assertThat(catalogManager.getAvailableWines()).hasSize(14);
+		assertThat(catalogManager.getUnavailableWines()).hasSize(0);
+	}
+
+	// Löscht komplett alle verfügbare Weinprodukte
+	@Test
+	public void deletesAllAvailableWines(){
+		Iterable<Wine> availableWines = catalogManager.getAvailableWines();
+		for (Wine wine : availableWines){
+			if (inventory.findByProduct(wine).isPresent()) {
+				inventory.delete(inventory.findByProduct(wine).get());
+			}
+			catalogManager.deleteById(wine.getProductId());
+		}
+
+		assertThat(catalogManager.getAllWines()).hasSize(5);
+		//assertThat(catalogManager.getAvailableWines()).hasSize(0);
+		assertThat(catalogManager.getUnavailableWines()).hasSize(5);
+	}
+}
